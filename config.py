41be6c82e0660c235ee25deba33cd4295f00dd63
VGmt <victor.guillemot@gmail.com>
2021-07-26 13:52:09 -0400

    vcap_services setup

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 import os
 import logging
+import json
 
 # Get configuration from environment
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
 )
+
+# Override when running in Cloud Foundry
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.environ['VCAP_SERVICES'])
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
