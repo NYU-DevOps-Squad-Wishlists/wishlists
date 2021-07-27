22556b5752c1c3a8d7fb8ca204a5a9ac5722fa8d
Billy Kern <william.j.kern@gmail.com>
2021-07-26 22:30:29 -0400

    add initial behave setup

---
+++ features/steps/wishlist_steps.py
@@ -0,0 +1,16 @@
+import behave
+
+
+@behave.given('we have behave installed')
+def step_impl(context):
+    pass
+
+
+@behave.when('we implement a test')
+def step_impl(context):
+    assert True is not False
+
+
+@behave.then('behave will test it for us!')
+def step_impl(context):
+    assert context.failed is False
